4e2fc71fad7772434c0d3b6d7d0aebc6ef0a3c42
juba-touam <jubatouam61@gmail.com>
2026-06-18 13:27:31 +0200

    fix(config): wip

--- src/main/java/com/ecommerce/paymentservice/dto/PaymentResponse.java
+++ src/main/java/com/ecommerce/paymentservice/dto/PaymentResponse.java
@@ -2,7 +2,6 @@ package com.ecommerce.paymentservice.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
--- src/main/java/com/ecommerce/paymentservice/filter/MdcRequestFilter.java
+++ src/main/java/com/ecommerce/paymentservice/filter/MdcRequestFilter.java
@@ -1,6 +1,10 @@
 package com.ecommerce.paymentservice.filter;
 
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.MDC;
 import org.springframework.core.Ordered;
